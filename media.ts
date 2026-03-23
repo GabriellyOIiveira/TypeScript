f359a0ba2b565020c3a7b32a87517f43c7a41d62
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2026-03-23 10:32:11 -0300

    media.ts

---
+++ media.ts
@@ -0,0 +1,12 @@
+//Imprimir a média de seis números;
+
+let a : number = 2
+let b : number = 4
+let c : number = 6
+let d : number = 8
+let e : number = 10
+let f : number = 12
+
+let media : number = (a+b+c+d+e+f)/6
+
+console.log("media")
