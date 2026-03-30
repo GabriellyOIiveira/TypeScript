513ff13f686dc279c0bee08b3eda9313f59f783a
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2026-03-30 10:59:14 -0300

    Create divisivel.ts

---
+++ divisivel.ts
@@ -0,0 +1,12 @@
+//Testar se um número é divisível por 3 e por 4 (tente fazer usando apenas if’s e else’s);
+let a: number = 5
+
+if (a%3==0){
+ if (a%4==0){
+ console.log("é divisível ")
+ } else {
+ console.log("não é divisível")
+ }
+}else{
+    console.log("não é divisível")
+}
