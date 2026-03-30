f4bda61323794371bd55dc2592402b1a1391151b
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2026-03-30 11:04:03 -0300

    Fix errors in student approval verification program
    Corrected syntax errors and improved variable declarations for student approval check.

---
+++ correçao.ts
@@ -0,0 +1,27 @@
+//CORRIGIDO:
+var media: number = 9
+var nome: string = "Carlos"
+
+
+if (media >= 6) {
+    console.log(nome+ " foi aprovado");
+} else {
+    console.log(nome+ " não foi aprovado");
+}
+
+//O programa abaixo deveria ser capaz de verificar se um estudante foi aprovado em uma determinada matéria com média mínima de 6 para a aprovação, mas possui diversos erros, corrija todos os erros do programa e para cada erro encontrado faça um comentário explicando qual o erro e qual foi a solução encontrada:
+
+var media = 7
+var nome = “João”
+if (media > 6) {
+    console.log(nome; "foi aprovado");
+} else if (media < 6) {
+    console.log(nome; 'não foi aprovado');
+}
+
+//Exemplo de entrada
+
+media = 9
+nome = “Carlos”
+
+/*Saída correspondente no console "Carlos foi aprovado"
