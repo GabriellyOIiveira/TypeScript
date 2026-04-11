756213dc86a417e03706f1470c07be056d4ad3b1
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2026-04-10 22:25:43 -0300

    Refactor function to identify the largest of three numbers
    Refactored the function to identify the largest of three numbers and removed the old implementation.

--- MaiorEntre3Numeros.ts
+++ MaiorEntre3Numeros.ts
@@ -1,22 +1,4 @@
 //atualização
-
-/*function verificar(num1: number, num2: number, num3: number){
-    if (num1>num2 && num1>num3){
-        return "O primeiro numero é o maior";
-    } else if (num2>num1 && num2>num3){
-        return "O segundo numero é o maior";
-    } else if (num3>num1 && num3>num2){
-        return "Terceiro numero é o maior";
-    } else{
-        return "Sao iguais";
-    }
-}
-console.log(verificar(13, 13, 13)); //iguais
-//(10, 3, 1) / primeiro numero é maior
-// (1, 13, 3) / segundo  numero é o maior
-// (3, 1, 13) / terceiro numero é o maior
-*/
-
 //Dados três números, encontrar o maior entre eles;
 
 function identificar(n1: number, n2:number, n3: number){
@@ -35,3 +17,21 @@ console.log(identificar( 8, 8, 8));
 //( 5, 8, 3) / "8, Segundo Numero é o maior" 
 //( 5, 8, 9) / "9, Terceiro Numero é o maior" 
 //( 8, 8, 8) / "São Iguais." 
+
+//antigo:
+/*function verificar(num1: number, num2: number, num3: number){
+    if (num1>num2 && num1>num3){
+        return "O primeiro numero é o maior";
+    } else if (num2>num1 && num2>num3){
+        return "O segundo numero é o maior";
+    } else if (num3>num1 && num3>num2){
+        return "Terceiro numero é o maior";
+    } else{
+        return "Sao iguais";
+    }
+}
+console.log(verificar(13, 13, 13)); //iguais
+//(10, 3, 1) / primeiro numero é maior
+// (1, 13, 3) / segundo  numero é o maior
+// (3, 1, 13) / terceiro numero é o maior
+*/
